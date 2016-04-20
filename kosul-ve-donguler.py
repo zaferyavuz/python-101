8905d4915f0cba39722b99698007a0c618161dfc
zaferyavuz <yavuzzafer@gmail.com>
2016-04-20 16:23:57 +0300

    Create kosul-ve-donguler.py

---
+++ kosul-ve-donguler.py
@@ -0,0 +1,8 @@
+# 1000 sayısından küçük, 3 ve 5 in katları olan sayıların toplamı
+i = 1; toplam = 0
+while i < 1000 :
+    if (i % 3 == 0) and (i % 5 == 0) :
+       print(i,end='\n')
+       toplam += i
+    i += 1
+print("Toplam",toplam,sep=':')
